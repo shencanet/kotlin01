3d6ba97716e8044a75276633d856175a55c3df99
shen <santi.dionis@gmail.com>
2023-03-27 19:40:27 +0200

    ADD: VARIABLES

---
+++ hola mundo.kt
@@ -0,0 +1,16 @@
+
+const val PI = 3.1416
+
+fun main(args: Array<String>){
+    println("Hello World")
+    var Dinero  = 10
+    println(Dinero)
+    Dinero= 5
+   println(Dinero)
+    val nombre = "Marta"
+    println(PI)
+    println(nombre)
+}
+val boolean : Boolean  = true
+val numeroLargo :  Long = 3927892789272987298729
+val numeroLargo2 :  Long = 3L
