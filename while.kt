b43813f241a81d9da96004487898841e31065801
shen <santi.dionis@gmail.com>
2023-03-29 21:06:03 +0200

    ADD: kotlin

--- while.kt
+++ while.kt
@@ -1,7 +1,17 @@
 fun main (){
-    var contador = 10
-    while (contador > 0){
-        println("el valor del contador es $contador")
-        contador --
-    }
+
+    fun main (){
+	
+        var  contador = 0
+            do {
+                
+                println("Intento generar numero aleatorio numero $contador")
+                val numeroAleatorio = (0..1000).random()
+                println("El numero aleatorio es $numeroAleatorio")
+                contador = contador +1
+                
+            }while(numeroAleatorio > 12)
+                
+        }
+        
 }
