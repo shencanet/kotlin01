17dba60eadca1ab41f0b06eb8a11586a769e3a3b
shen <santi.dionis@gmail.com>
2023-03-29 19:19:00 +0200

    ADD: kotlin

---
+++ when.kt
@@ -0,0 +1,33 @@
+fun main (){
+    val nombreColor = "rojo"
+
+    when(nombreColor) {
+        "rojo" -> println("El color es rojo")
+        "verde" -> println("El color es verde")
+        "amarillo" -> println("El color es amarillo")
+        else -> println("El color no es rojo, verde o amarillo")
+    }
+}
+
+
+fun main(args: Array<String>) {
+
+    // For
+    val listaDeFrutas = listOf("Manzana","Pera","Piña","Frambuesa")
+    for (fruta in listaDeFrutas) println(fruta)
+
+    //For Each
+    listaDeFrutas.forEach{
+        println("El nombre de la fruta es: $it ")
+    }
+
+    //Map
+    val nuevaLista = listaDeFrutas.map { it.length }
+    println(nuevaLista)
+
+
+    //Filter
+    val listaFiltrada = nuevaLista.filter { it > 4 }
+    println(listaFiltrada)
+
+}
