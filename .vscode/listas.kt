f97831dbefbe688183ad37d8a8114bb22a5a8fe2
shen <santi.dionis@gmail.com>
2023-03-30 21:09:43 +0200

    ADD: kotlin

---
+++ .vscode/listas.kt
@@ -0,0 +1,6 @@
+val readOnly: List<String> = listOf("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo")
+readOnly.size //Muestra el tamaño de la lista
+readOnly.get(3) //Devuelve el valor de la posición 3
+readOnly.first() //Devuelve el primer valor
+readOnly.last() //Devuelve el último valor
+println(readOnly) //[Lunes, Martes, Miércoles, Jueves, Viernes, Sábado, Domingo]
