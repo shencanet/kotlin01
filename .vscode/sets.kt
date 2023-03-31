0a7731a66ca0cc34701f70e2ff31bba5f2cd73d8
shen <santi.dionis@gmail.com>
2023-03-31 21:17:27 +0200

    ADD: kotlin

---
+++ .vscode/sets.kt
@@ -0,0 +1,21 @@
+//solo puede tener un elemento igual el segundo se elimina
+//sets mutables y inmutables
+fun main(args: Array<String>) {
+
+    //Creando un set inmutable
+    val vocalesRepetidas = setOf("a", "e", "i", "o", "u", "a", "e", "i", "o", "u")
+    println(vocalesRepetidas) // Resultado: [a, e, i, o, u]
+
+    //Creando set mutables
+    val numerosFavoritos = mutableSetOf(1,2,3,4)
+    numerosFavoritos.add(5)
+    println(numerosFavoritos) // Resultado: [1, 2, 3, 4, 5]
+
+    //Eliminado elementos de set
+    numerosFavoritos.remove(3)
+    println(numerosFavoritos) // Resultado: [1, 2, 4, 5]
+
+    //Eliminar dependiendo de una condición
+    numerosFavoritos.removeIf { numero -> numero.rem(2) != 0 }
+    println(numerosFavoritos)
+}
