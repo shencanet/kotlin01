1f7f38a5065dc37878de42ff536b190408b4ccae
shen <santi.dionis@gmail.com>
2023-04-01 21:29:22 +0200

    ADD: kotlin

---
+++ .vscode/Funcion.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    showMyName()
+    showMyLastName()
+    showMyAge()
+}
+fun showMyName(){
+    println("Me llamo Santi")
+}
+fun showMyLastName(){
+    println("Mi Apellido es Dionis")
+}
+fun showMyAge(){
+    println("Tengo 24 años")
+}
+/*Las funciones se declaran usando la palabra clave fun, seguida del nombre del método, los paréntesis donde declararemos los valores de entrada y unas llaves que limitan la función. */
