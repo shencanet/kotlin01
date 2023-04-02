3aed3ff22a788d769cfb08cf6f0cf63b2443b5b4
shen <santi.dionis@gmail.com>
2023-04-02 10:59:00 +0200

    ADD: kotlin

--- .vscode/Tipo Parametros.kt
+++ .vscode/Tipo Parametros.kt
@@ -7,3 +7,50 @@ fun sum(x:Int, y:Int, z:Int): Int{
  fun printWholeName(firstName: String, secondName: String = "", surName: String) {
     println("My name is $firstName ${if (secondName.isNotEmpty()) "$secondName " else ""}$surName")
 }
+
+
+fun main(args: Array<String>) {
+
+    // Let
+    var nombre: String? = null
+    nombre?.let { // Esto no se ejecutará
+        println("El nombre no es nulo, es $it")
+    }
+    nombre = "Giuseppe"
+    nombre?.let { // Esto se ejecutará
+        println("El nombre no es nulo, es $it")
+    }
+
+    //With
+    val usuario = "giuseppe@platzi.com"
+    val mensajeDeInicioSesion = with(usuario) {
+        iniciarSesion(this)
+        "Bienvenido $this"
+    }
+    println(mensajeDeInicioSesion)
+
+    //Run
+    val moviles = mutableListOf("Google Pixel 2XL","Google Pixel 4a","Huawei Redmi 9","Xiaomi mi A3").run {
+        removeIf { movil -> movil.contains("Google") }
+        this
+    }
+    println(moviles)
+
+    //Apply
+    val movilesSinGoogle = mutableListOf("Google Pixel 2XL","Google Pixel 4a","Huawei Redmi 9","Xiaomi mi A3").apply{
+            removeIf { movil -> movil.contains("Google") }
+        }
+    println(movilesSinGoogle)
+
+    //Also
+    val lista = mutableListOf("Google Pixel 2XL","Google Pixel 4a","Huawei Redmi 9","Xiaomi mi A3")
+        .also { lista -> println("Lista original $lista") }
+        .asReversed()
+
+    println(lista)
+
+}
+
+fun iniciarSesion(usuario: String){
+    println("Iniciando sesión con usuario $usuario")
+}
